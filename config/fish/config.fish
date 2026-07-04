83acf1ee5b656d501ffec23a8f3777465b4870b6
ndtduy <76892496+ndtduy@users.noreply.github.com>
2026-07-04 16:27:40 +0700

    Modified terminal & shell config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,23 @@
+# if status is-interactive
+#     # Commands to run in interactive sessions can go here
+#     set fish_greeting
+#     starship init fish | source
+#     set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
+#     # cat ~/pokeget.txt
+# end
+
+set -gx FISH_PROMPT starship
+# set -g FISH_PROMPT oh-my-posh
+
+set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set fish_greeting
-    starship init fish | source
-    set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
-    cat ~/pokeget.txt
+    # Initialize prompt (once — do NOT reinitialize on events)
+    switch $FISH_PROMPT
+        case starship
+            starship init fish | source
+        case oh-my-posh
+            oh-my-posh init fish --config ~/.config/tokyo.omp.json | source
+    end
+    # cat ~/pokeget.txt
 end
